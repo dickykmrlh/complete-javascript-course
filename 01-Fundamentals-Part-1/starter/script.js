6e70cd5462e9cc2afeac99955fab4ddbbf2834e3
Dicky Kamarulah <dicky@munalively.com>
2022-02-14 18:54:27 +0700

    completed code chalange

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,16 +1,25 @@
-let country = "Indonesia"
-const continent = "Asia"
-let population = 273.5
+const markWeightData1 = 78
+const markHeightData1 = 1.69
+const johnWeightData1 = 92
+const johnHeightData1 = 1.95
 
-console.log(country)
-console.log(continent)
-console.log(population)
+const markBMIData1 = markWeightData1 / markHeightData1 ** 2
+const johnBMIData1 = johnWeightData1 / johnHeightData1 ** 2
+const markHigherBMIDat1 = markBMIData1 > johnBMIData1
 
-const isIsland = true
-let language = "Indonesia"
+console.log(markBMIData1)
+console.log(johnBMIData1)
+console.log(markHigherBMIDat1)
 
-console.log(typeof country)
-console.log(typeof continent)
-console.log(typeof population)
-console.log(typeof isIsland)
-console.log(typeof language)
+const markWeightData2 = 95
+const markHeightData2 = 1.88
+const johnWeightData2 = 85
+const johnHeightData2 = 1.76
+
+const markBMIData2 = markWeightData2 / markHeightData2 ** 2
+const johnBMIData2 = johnWeightData2 / johnHeightData2 ** 2
+const markHigherBMIDat2 = markBMIData2 > johnBMIData2
+
+console.log(markBMIData2)
+console.log(johnBMIData2)
+console.log(markHigherBMIDat2)
